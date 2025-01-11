3d1432b3266e437c8c67e06fdccb90ee05799bb9
thfabricio <thiago.fabricio123@gmail.com>
2025-01-11 19:48:14 -0300

    feat: criacao do model de categoria

---
+++ desafio-api/desafio-api/Domain/Models/Category.cs
@@ -0,0 +1,29 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.InteropServices.JavaScript;
+
+namespace desafio_api.Domain.Models;
+
+[Table("category")]
+public class Category
+{
+ 
+    [Key]
+    public Guid id { get; private set; }
+    public string title { get; private set; }
+    public string description { get; private set; }
+    public string code { get; private set; }
+    public DateTime created_at { get; private set; }
+    public DateTime? updated_at { get; private set; }
+    public DateTime? deleted_at { get; private set; }
+    
+    public Category(string title, string description, string code)
+    {
+        this.id = Guid.NewGuid();
+        this.title = title;
+        this.description = description;
+        this.code = code;
+        this.created_at = DateTime.Now;
+    }
+    
+}
