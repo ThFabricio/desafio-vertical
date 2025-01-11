d5f458118f6bc0fe2d341624297989702b5fb3d8
thfabricio <thiago.fabricio123@gmail.com>
2025-01-11 19:52:38 -0300

    feat: instalacao da biblioteca Microsoft.EntityFrameworkCore.Tools

--- desafio-api/desafio-api/obj/Debug/net8.0/desafio-api.AssemblyInfo.cs
+++ desafio-api/desafio-api/obj/Debug/net8.0/desafio-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("desafio-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db7ac0e61c7b1f8f34ee685ff6c7fe0d758cb50a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d1432b3266e437c8c67e06fdccb90ee05799bb9")]
 [assembly: System.Reflection.AssemblyProductAttribute("desafio-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("desafio-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
