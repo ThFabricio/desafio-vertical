ba3a3e893042d09aa61e271978aef31da5062ba9
thfabricio <thiago.fabricio123@gmail.com>
2025-01-12 17:50:23 -0300

    feat: criacao do DTO de Category

---
+++ desafio-api/desafio-api/Domain/DTO/CategoryDTO.cs
@@ -0,0 +1,9 @@
+namespace desafio_api.Domain.DTO;
+
+public class CategoryDTO
+{
+    public Guid id { get; set; }
+    public string title { get; set; }
+    public string description { get; set; }
+    public string code { get; set; }
+}
