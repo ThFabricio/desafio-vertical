87a535b6616e25429d2352b65634158f7e4a921c
thfabricio <thiago.fabricio123@gmail.com>
2025-01-12 20:24:08 -0300

    feat: adicionando validação para entradas de dados das requisições

--- desafio-api/desafio-api/Application/ViewModel/CategoryViewModel.cs
+++ desafio-api/desafio-api/Application/ViewModel/CategoryViewModel.cs
@@ -1,8 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace desafio_api.Application.ViewModel;
 
 public class CategoryViewModel
 {
+    [Required (ErrorMessage = "Title is required")]
+    [StringLength(100, ErrorMessage = "Title must have a maximum of 100 characters")]
     public string title { get; set; }
+    [Required (ErrorMessage = "Description is required")]
+    [StringLength(255, ErrorMessage = "Description must have a maximum of 255 characters")]
     public string description { get; set; }
+    [Required (ErrorMessage = "Code is required")]
+    [RegularExpression(@"^[a-zA-Z0-9]*$", ErrorMessage = "Code must have only letters and numbers")]
     public string code { get; set; }
 }
