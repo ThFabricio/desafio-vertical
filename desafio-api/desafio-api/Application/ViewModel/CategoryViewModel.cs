580c0384a9fd97e6614384738af74446180c59da
thfabricio <thiago.fabricio123@gmail.com>
2025-01-12 15:21:31 -0300

    feat: criação do categoryViewModel

---
+++ desafio-api/desafio-api/Application/ViewModel/CategoryViewModel.cs
@@ -0,0 +1,8 @@
+namespace desafio_api.Application.ViewModel;
+
+public class CategoryViewModel
+{
+    public string title { get; set; }
+    public string description { get; set; }
+    public string code { get; set; }
+}
