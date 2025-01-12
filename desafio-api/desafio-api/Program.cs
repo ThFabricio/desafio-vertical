d60267e610d78c71940d4130417a5e5c6aee171b
thfabricio <thiago.fabricio123@gmail.com>
2025-01-12 15:04:26 -0300

    feat: adicionando injeção de dependencia no Program.cs

--- desafio-api/desafio-api/Program.cs
+++ desafio-api/desafio-api/Program.cs
@@ -1,3 +1,6 @@
+using desafio_api.Infrastructure.Repository;
+using desafio_api.Infrastructure.Repository.Interface;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddTransient<ICategoryRepository, CategoryRepository>();
 
 var app = builder.Build();
 
