69d198abbbe954c6c042064b5b55cb14365e1a5e
thfabricio <thiago.fabricio123@gmail.com>
2025-01-12 18:01:27 -0300

    fix: remocação do CategoryDTO

--- desafio-api/desafio-api/Domain/DTO/CategoryDTO.cs
+++
@@ -1,9 +0,0 @@
-namespace desafio_api.Domain.DTO;
-
-public class CategoryDTO
-{
-    public Guid id { get; set; }
-    public string title { get; set; }
-    public string description { get; set; }
-    public string code { get; set; }
-}
