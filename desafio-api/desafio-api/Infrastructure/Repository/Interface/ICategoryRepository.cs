3ad34e2ee49a72ee3f395564e2a95c4f9c049cfd
thfabricio <thiago.fabricio123@gmail.com>
2025-01-12 15:01:12 -0300

    feat: criando a interface de repositorio de categoria

---
+++ desafio-api/desafio-api/Infrastructure/Repository/Interface/ICategoryRepository.cs
@@ -0,0 +1,10 @@
+using desafio_api.Domain.Models;
+
+namespace desafio_api.Infrastructure.Repository.Interface;
+
+public interface ICategoryRepository
+{
+    void CreateCategory(CategoryModel category);
+
+    List<CategoryModel> getCategoryModels();
+}
